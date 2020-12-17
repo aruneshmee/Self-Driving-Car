b7081cf20cfcb8dd23cbd6852b6f53f9823d9738
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2020-12-17 14:51:50 +0530

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,80 @@
+# Importing important libraries
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import keras
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense
+import cv2
+import pandas as pd
+import random
+import ntpath
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import matplotlib.image as mpimg
+
+# Defining columns
+columns = ['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed']
+data = pd.read_csv('track/driving_log.csv', names = columns)
+pd.set_option('display.max_colwidth', -1)
+data.head(5)
+
+# Assigning values to the images 
+def path_leaf(path):
+  head, tail = ntpath.split(path)
+  return tail
+
+data['center'] = data['center'].apply(path_leaf)
+data['left'] = data['left'].apply(path_leaf)
+data['right'] = data['right'].apply(path_leaf)
+
+# Defining bins and samples
+num_bins = 25
+samples_per_bin = 200
+
+hist, bins = np.histogram(data['steering'], num_bins)
+center = (bins[:-1] + bins[1:])*0.5
+
+plt.bar(center, hist, width=0.05)
+plt.plot((np.min(data['steering']), np.max(data['steering'])), (samples_per_bin, samples_per_bin))
+
+# Balancing Data
+print('total data: ', len(data))
+remove_list = []
+
+for j in range(num_bins):
+  list_ = []
+  for i in range(len(data['steering'])):
+    if data['steering'][i] >= bins[j] and data['steering'][i] <= bins[j+1]:
+      list_.append(i)
+  list_ = shuffle(list_)
+  list_ = list_[samples_per_bin:]
+  remove_list.extend(list_)
+
+print('removed ',len(remove_list))
+data.drop(data.index[remove_list], inplace=True)
+print('remainign ', len(data))
+
+hist, _ = np.histogram(data['steering'], (num_bins))
+plt.bar(center, hist, width=0.05)
+plt.plot((np.min(data['steering']), np.max(data['steering'])), (samples_per_bin, samples_per_bin))
+
+# Getting steering angle from the datalog
+def load_steering(datadir, df):
+
+  image_path = []
+  steering = []
+
+  for i in range(len(data)):
+    indexed_data = data.iloc[i]
+    center, left, right = indexed_data[0], indexed_data[1], indexed_data[2]
+
+    image_path.append(os.path.join(datadir, center.strip()))
+    steering.append(float(indexed_data[3]))
+
+  image_paths = np.asarray(image_path)
+  steerings = np.asarray(steering)
+  return image_paths, steerings
+
+image_paths, steerings = load_steering(datadir+'/IMG', data)
